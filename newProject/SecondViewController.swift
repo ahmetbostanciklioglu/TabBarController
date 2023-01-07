c7724504c4361c563cd42995ee70b2d25381f2a2
AhmetBostanciklioglu <ahmet.bostancikli@arafas.com>
2023-01-07 18:24:14 +0300

    Initial Commit

---
+++ newProject/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  newProject
+//
+//  Created by Ahmet Bostancıklıoğlu on 3.01.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+    @IBAction func button(_ sender: Any) {
+        
+        
+    }
+    
+}
